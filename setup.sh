8c92eb5dfd376be8f146d46271f3dfca26d07bef
LbeozTJ <151171561+LbeozTJ@users.noreply.github.com>
2026-05-10 21:56:30 -0500

    chore: Add .gitignore, setup.sh, requirements.txt for Middara PC Game

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Middara PC Game Setup - Personal Edition
+echo "Setting up Middara Digital Edition..."
+pip install -r requirements.txt
+echo "Done. Run with: python main.py"
+echo "Remember to copy your personal artwork from Google Drive Middara folder into ./assets/"
